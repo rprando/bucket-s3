400d8d0ac85412e38ee1dc9768f7572b87cdd5a3
Prando <rodrigo-prando@hotmail.com>
2023-06-25 22:20:55 -0300

    Removendo linhas não usadas

--- app/src/CopiarArquivos.py
+++ app/src/CopiarArquivos.py
@@ -30,21 +30,3 @@ def process_file(file_path):
 def upload_file_to_s3_client(bucket_name, key, file_path):
     s3_client = boto3.client('s3')
     s3_client.upload_file(file_path, bucket_name, key)
-
-# # Defina as informações dos buckets e dos arquivos
-# bucket_origem = 'dados'
-# key_origem = 'tb_001/arquivo.csv'
-# bucket_destino = 'bucket-sor'
-# key_destino = 'arquivo_tratado.csv'
-
-# # Define o caminho de destino na pasta /tmp do Lambda
-# caminho_destino = '/tmp/arquivo.csv'
-
-# # Faz o download do arquivo do bucket de origem
-# download_file_from_s3_client(bucket_origem, key_origem, caminho_destino)
-
-# # Trata o arquivo removendo as aspas duplas
-# process_file(caminho_destino)
-
-# # Salva o arquivo tratado no bucket de destino
-# upload_file_to_s3_client(bucket_destino, key_destino, caminho_destino)
